55fb5753d99ad75cc128481b8f3169231507bcb6
Kaio <kaiobarb@gmail.com>
2025-11-08 17:05:02 -0800

    theme: prefer system default

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="dark"
-      enableSystem={false}
+      defaultTheme="system"
+      enableSystem={true}
       {...props}
     >
       {children}
